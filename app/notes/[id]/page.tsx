6f6cdb2b474306b65055827d854e2d2b315be041
Roksolana <adelaw283@gmail.com>
2025-08-06 22:47:03 +0300

    page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,22 +2,20 @@ import { fetchNoteById } from "@/lib/api";
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import NoteDetailsClient from "./NoteDetails.client";
 
-type PageProps = {
-  params: Promise<{ id: string }>;
+export default async function NoteDetails({ params }: { params: { id: string } }) {
+  const { id } = params;
+
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NoteDetailsClient id={id} />
+    </HydrationBoundary>
+  );
 }
 
-export default async function NoteDetails({ params }: PageProps) {
-    const { id } = await params;
-    const queryClient = new QueryClient();
-
-    await queryClient.prefetchQuery({
-        queryKey: ["note", id],
-        queryFn: () => fetchNoteById(id),
-    });
-
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <NoteDetailsClient id={id} />
-        </HydrationBoundary>
-    );
-}
